e2fe60f1c0eb61557593a6ae8693fd42006b6ba8
huangxin.maigo <huangxin.maigo@bytedance.com>
2019-05-15 16:02:46 +0800

    更新demo

--- examples/DemoVodPlay.php
+++ examples/DemoVodPlay.php
@@ -2,7 +2,7 @@
 require('../vendor/autoload.php');
 use Vcloud\Service\Vod;
 
-$vid = "your vid";
+$vid = "your-vid";
 
 echo "\nstaging 获取播放地址\n";
 $response = Vod::getInstance()->request('GetPlayInfo', ['query' => ['video_id' => $vid]]);
