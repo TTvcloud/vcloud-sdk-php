9ce3d90356bb5065681b45d837ca203be9f37826
huangxin.maigo <huangxin.maigo@bytedance.com>
2019-05-22 10:44:34 +0800

    更新sample

--- examples/DemoVodPublish.php
+++ examples/DemoVodPublish.php
@@ -6,5 +6,5 @@ $vid = "your vid";
 $space = "your space";
 
 echo "\n修改发布状态\n";
-$response = Vod::getInstance()->request('SetVideoPublishStatus', ['json' => ['Vid' => $vid, 'SpaceName' => $space]]);
+$response = Vod::getInstance()->request('SetVideoPublishStatus', ['json' => ['Vid' => $vid, 'SpaceName' => $space, 'Status' => 'Published']]);
 echo (string)$response->getBody();
