d1d5ae84ed13a90f6d06d46bf42a116e545e4ddd
huangxin.maigo <huangxin.maigo@bytedance.com>
2019-06-26 11:32:45 +0800

    更新发布的demo

--- examples/DemoVodTranscode.php
+++ examples/DemoVodTranscode.php
@@ -4,8 +4,8 @@ use Vcloud\Service\Vod;
 
 $vid = "your vid";
 $space = "your space";
-$tip = "your template id";
+$tid = "your template id";
 
 echo "\n修改发布状态\n";
-$response = Vod::getInstance()->request('StartTranscode', ['query' => ['TemplateId' => $tid], 'json' => ['Vid' => $vid, 'Input' => []]]);
+$response = Vod::getInstance()->request('StartTranscode', ['query' => ['TemplateId' => $tid], 'json' => ['Vid' => $vid]]);
 echo (string)$response->getBody();
