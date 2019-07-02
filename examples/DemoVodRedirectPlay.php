3d545814c0ac22934fe1c5bd9e6d3871a41e23cb
huangxin.maigo <huangxin.maigo@bytedance.com>
2019-07-02 15:02:26 +0800

    调整RedirectUrl的播放demo

--- examples/DemoVodRedirectPlay.php
+++ examples/DemoVodRedirectPlay.php
@@ -3,6 +3,7 @@ require('../vendor/autoload.php');
 use Vcloud\Service\Vod;
 
 $vid = "your-vid";
+$expire = 3600; // 请求的签名有效期
 
-$string = Vod::getInstance()->getRequestUrl('RedirectPlay', ['query' => ['video_id' => $vid]]);
+$string = Vod::getInstance()->getRequestUrl('RedirectPlay', ['query' => ['video_id' => $vid, 'X-Amz-Expires' => $expire]]);
 echo $string;
